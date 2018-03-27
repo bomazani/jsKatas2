af28412ae2b392e5c77b978c7a444e7512901bc9
Bob Hunt <bob.hunt.email@gmail.com>
2018-03-27 17:40:12 -0400

    working on 6th kata

--- jsKatas2.js
+++ jsKatas2.js
@@ -31,7 +31,49 @@ console.log(minValue(["Edwin", "Bob"]));
 
 
 // Find the maximum value in an array
+function maxValue(array){
+    let largest=array[0];
+    for (let i=1; i<array.length; i++){
+        if (array[i]>largest){
+            largest=array[i];
+        }
+    }
+    return largest;
+}
+
+console.log(maxValue([8, 1, 4, 9]));
+console.log(maxValue(["Edwin", "Bob"]));
+
 // Calculate a remainder (given a numerator and denominator)
+function getRemainder(numerator,denominator){
+    let quotient = Math.floor(numerator/denominator);
+    let remainder = (numerator-(Math.floor(numerator/denominator))*denominator);
+    return remainder;
+}
+console.log(getRemainder(17,5));
+console.log(getRemainder(13,3));
+
 // Return distinct values from a list including duplicates (i.e. "1 3 5 3 7 3 1 1 5" -> "1 3 5 7")
+let list1 = "1 3 5 3 7 3 1 1 5"
+let list2 = "Bob, Edwin, Morgan, Mike, Bob, Stan, Kavitha, Declan, Kavitha, David, Morgan, Bob"
+function getDistinctValues(list){
+    let distinctVals = [];
+    let valueList = list.split(" ");
+    console.log(list);
+    console.log(valueList);
+    
+    for (let i=0; i<valueList.length; i++){
+        if (distinctVals.includes(valueList[i])){
+            i++;
+        }else{
+            distinctVals.push(valueList[i])
+        } 
+    }
+    return distinctVals;
+}
+
+console.log(getDistinctValues(list1));
+console.log(getDistinctValues(list2));
+
 // Return distinct values and their counts (i.e. the list above becomes "1(3) 3(3) 5(2) 7(1)")
 // Given a string of expressions (only variables, +, and -) and an object describing a set of variable/value pairs like {a: 1, b: 7, c: 3, d: 14}, return the result of the expression ("a + b+c -d" would be -3).
