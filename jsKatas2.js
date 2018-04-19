4c728a5bfca50e62a79d746d554f1311c028afbf
Bob Hunt <bob.hunt.email@gmail.com>
2018-04-19 14:22:41 -0400

    commit

--- jsKatas2.js
+++ jsKatas2.js
@@ -42,9 +42,10 @@ function maxValue(array){
     }
     return largest;
 }
-
 console.log(maxValue([8, 1, 4, 9]));
 console.log(maxValue(["Edwin", "Bob"]));
+
+
 
 // 5) Calculate a remainder (given a numerator and denominator)
 function getRemainder(numerator,denominator){
@@ -54,8 +55,19 @@ function getRemainder(numerator,denominator){
 }
 console.log(getRemainder(17,5));
 console.log(getRemainder(13,3));
+    // or even better
 
+function getRemainder2(numberator,denominator){
+    let remainder = numberator % denominator;
+    return remainder; 
+}
+console.log(getRemainder(18,5));
+console.log(getRemainder(14,3));
+    // or even more betterer
 
+const getRemainder3 = (numberator, denominator) => {return numberator % denominator};
+console.log(getRemainder3(9,5));
+console.log(getRemainder3(10,3));
 
 // 6) Return distinct values from a list including duplicates (i.e. "1 3 5 3 7 3 1 1 5" -> "1 3 5 7")
 let list1 = "1 3 9 5 3 7 3 1 1 8 5"
@@ -82,27 +94,28 @@ console.log(getDistinctValues(list2));
 
 
 // 7) Return distinct values and their counts (i.e. the list above becomes "1(3) 3(3) 5(2) 7(1)")
-    let list3 = "1 3 9 5 3 7 3 1 1 8 5"
+    // let list3 = "1 3 9 5 3 7 3 1 1 8 5"
     let list4 = "Bob, Edwin, Morgan, Mike, Bob, Stan, Kavitha, Declan, Kavitha, David, Morgan, Bob"
-    function getDistinctValues(list){
-        let distinctVals = [];
-        let valueList = list.split(" ");
-        // console.log(list);
-        // console.log(valueList);
+    function getDistinctValues7(list){
+        let distinctVals7 = {};
+        let valueList7 = list.split(" ");
+        console.log(list);
+        console.log(valueList7);
         
-        for (let i=0; i<valueList.length; i++){
-            if (distinctVals.includes(valueList[i])){
-                
+        for (let i=0; i<valueList7.length; i++){
+            if (distinctVals7[valueList7[i]] !== null){
+                distinctVals7[valueList7[i]] += 1;               
             }else{
-                // distinctVals.push(valueList[i])
-
+                distinctVals7[valueList7[i]] = 1;
             } 
         }
-        return distinctVals;
+        console.log(distinctVals7)
+        let distVals7 = distinctVals7.values;
+        return distVals7;
     }
     
-    console.log(getDistinctValues(list3));
-    console.log(getDistinctValues(list4));
+    // console.log(getDistinctValues7(list3));
+    console.log(getDistinctValues7(list4));
  
 
 
